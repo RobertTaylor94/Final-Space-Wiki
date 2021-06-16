d52674652d4ba5b181913c5164e539780c5a127d
RobertTaylor94 <rjt903@gmail.com>
2021-06-16 11:20:52 +0100

    Updated collectionView

---
+++ Final Space Wiki/View/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Final Space Wiki
+//
+//  Created by Robert Taylor on 15/06/2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var charImage: UIImageView!
+    @IBOutlet weak var charName: UILabel!
+    
+    
+    
+}
