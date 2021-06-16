931095a846096a22038dd1b1c1f2fbed09c91640
RobertTaylor94 <rjt903@gmail.com>
2021-06-16 21:11:33 +0100

    Added LocationManager and Tableview to view location data

--- Final Space Wiki/Controller/LocationViewController.swift
+++ Final Space Wiki/Controller/LocationViewController.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
-class LocationViewController: UIViewController {
+class LocationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, LocationManagerDelegate {
+    
+    var locationManager = LocationManager()
+    var locations = [Location]()
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.loadLocationData()
+        tableView.delegate = self
+        tableView.dataSource = self
+        locations = locationManager.locations
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! LocationViewCell
+        
+        cell.locationNameLbl.text = locations[indexPath.row].name
+        cell.locationTypeLbl.text = locations[indexPath.row].type
+        cell.locationInhabitantsLbl.text = "\(locations[indexPath.row].inhabitants)"
+        cell.locationImg.sd_setImage(with: URL(string: locations[indexPath.row].img_url))
+        
+        
+        return cell
+    }
+    
+    func didRecieveDataUpdate(data: [Location]) {
+        self.locations = data
+        self.tableView.reloadData()
+    }
 
 }
---
+++ Final Space Wiki/View/LocationViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodeViewCell.swift
+//  Final Space Wiki
+//
+//  Created by Robert Taylor on 16/06/2021.
+//
+
+import Foundation
+import UIKit
+
+class LocationViewCell: UITableViewCell {
+    
+    @IBOutlet weak var locationImg: UIImageView!
+    @IBOutlet weak var locationNameLbl: UILabel!
+    @IBOutlet weak var locationTypeLbl: UILabel!
+    @IBOutlet weak var locationInhabitantsLbl: UILabel!
+}
